78d17f620a60c67316b16d2735e0e3bad869f177
dong13475 <travel1347@gmail.com>
2023-02-13 15:30:02 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -69,5 +69,39 @@ window.addEventListener("DOMContentLoaded", ()=>{
     anibx.classList.remove("on");
   };
   ///// click 함수 /////
+
+
+  /******************************************** 
+    SNS버튼 링크 이동하기
+  ********************************************/
+  // 1. 대상선정 : .sns a
+  const sns = qsa(".sns a");
+  // 2. 클릭 이벤트 설정
+  // forEach((요소자신,순번,객체)=>{코드});
+  sns.forEach((ele)=>{
+    ele.onclick = ()=>{
+      // 1. 내부텍스트
+      let btxt = ele.innerText;
+      
+      // console.log(btxt);
+
+      // 이동주소 변경
+      let url="";
+      // 2. 분기문 url할당하기
+      switch(btxt){
+        case "페이스북 바로가기": url="https://www.facebook.com/CJCGV"; break;
+        case "트위터 바로가기": url="https://twitter.com/CGV_ID"; break;
+        case "인스타그램 바로가기": url="https://www.instagram.com/cgv_korea/"; break;
+      }; ///// switch case 문 /////
+
+      // 3. 페이지 이동하기 : 새창으로!
+      // 새창 -> window.open()
+      // 페이지이동 -> location.href = url값
+      window.open().location.href = url;
+      
+    }; ///// click 함수 /////
+  }); ///// forEach 문 /////
+  
+  
   
 }); ////////// 로드구역 //////////
