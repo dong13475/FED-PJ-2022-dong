66a64824fe97082f6206c8d1ad632b3ddc87c4e4
dong13475 <travel1347@gmail.com>
2023-02-21 00:08:05 +0900

    Update 01.가로방향슬라이드연습.js

--- 004.JS학습/JS연습/01.가로방향슬라이드연습.js
+++ 004.JS학습/JS연습/01.가로방향슬라이드연습.js
@@ -3,19 +3,19 @@ function loadFn(){
   console.log("로딩완료!");
   let snum = 0;
   let scnt = document.querySelectorAll("#slide>li").length;
+  console.log("슬라이드개수",scnt);
   const abtn = document.querySelectorAll(".abtn");
   const slide = document.querySelector("#slide");
   const goSlide = (seq) => {
-    console.log("슬고우!",seq);
     if(seq){
       snum++;
-      console.log("오른!",snum);
+      console.log("오른쪽!",snum);
     }
     else{
       snum--;
       console.log("왼쪽!",snum);
     }
-    if(snum===-1) snum = scnt-1;
+    if(snum === -1) snum = scnt - 1;
     else if(snum === scnt) snum = 0;
     slide.style.left = (snum*-100)+"%";
     slide.style.transition = "left .4s ease-in-out";
