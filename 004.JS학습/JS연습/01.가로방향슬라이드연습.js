76dbb4d8e5c2517e4f92408acdb628847ab8e149
dong13475 <travel1347@gmail.com>
2023-02-20 23:48:14 +0900

    학습

--- 004.JS학습/JS연습/01.가로방향슬라이드연습.js
+++ 004.JS학습/JS연습/01.가로방향슬라이드연습.js
@@ -1,40 +1,28 @@
-// loadFn 함수 호출 
 window.addEventListener("DOMContentLoaded",loadFn);
-// 로드구역
-function loanFn(){
-  // 슬라이드번호 변수
+function loadFn(){
+  console.log("로딩완료!");
   let snum = 0;
-  // 슬라이드 개수 변수
   let scnt = document.querySelectorAll("#slide>li").length;
-
   const abtn = document.querySelectorAll(".abtn");
   const slide = document.querySelector("#slide");
-  // 슬라이드 변경함수
   const goSlide = (seq) => {
-    // 방향에 따른분기
-    // 오른쪽버튼 클릭시 : seq===1일경우
+    console.log("슬고우!",seq);
     if(seq){
-      // 슬라이드 번호 증가
       snum++;
+      console.log("오른!",snum);
     }
-    // 왼쪽버튼 클릭시 : seq===0일경우
     else{
-      // 슬라이드 번호 감소
       snum--;
+      console.log("왼쪽!",snum);
     }
-    // 한계값 체크
-    // 처음이전 -> 끝
     if(snum===-1) snum = scnt-1;
-    // 끝다음 -> 처음
-    else if(snum === scnt) snum =0;
-    // 이동하기
+    else if(snum === scnt) snum = 0;
     slide.style.left = (snum*-100)+"%";
     slide.style.transition = "left .4s ease-in-out";
   };
-  // 대상에 이벤트 설정하기
   abtn.forEach((ele,idx)=>{
     ele.onclick = () => {
       goSlide(idx);
     };
   });
-} // 로드구역 // 
+}
