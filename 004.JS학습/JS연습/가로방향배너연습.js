3bd6f210edc1c0e30c247c047f0532521d28cfc7
dong13475 <travel1347@gmail.com>
2023-02-22 17:20:55 +0900

    학습

--- 004.JS학습/JS연습/가로방향배너연습.js
+++ 004.JS학습/JS연습/가로방향배너연습.js
@@ -1,20 +1,25 @@
 window.addEventListener("DOMContentLoaded",loadFn);
 function loadFn(){
+
   let slist = document.querySelectorAll("#slide>li");
   slist.forEach((ele,idx)=>{
     ele.setAttribute("data-seq",idx);
-  }); ///// forEach //////
+  });
+
   const abtn = document.querySelectorAll(".abtn");
-  const slide = document.querySelector("slide");
+  const slide = document.querySelector("#slide");
   const indic = document.querySelectorAll(".indic li");
   let prot = 0;
+
   const goSlide = (seq) => {
     if(prot) return;
     prot = 1;
     setTimeout(()=>{
       prot = 0;
     },400);
+
     let clist = slide.querySelectorAll("li");
+
     if(seq){
       slide.style.left = "-100%";
       slide.style.transition = "left .4s ease-in-out";
@@ -22,25 +27,43 @@ function loadFn(){
         slide.appendChild(clist[0]);
         slide.style.left = "0";
         slide.style.transition = "none";
-      },400); /////// 타임아웃 //////////
-    } ////////// if //////////
-    else {
+      },400);
+    }
+    else{
       slide.insertBefore(clist[clist.length-1],clist[0]);
       slide.style.left = "-100%";
       slide.style.transition = "none";
       setTimeout(()=>{
         slide.style.left = "0";
         slide.style.transition = "left .4s ease-in-out";
-      },0); ////// 타임아웃 /////////
-    } //////// else //////////
+      },0);
+    }
+
     clist = slide.querySelectorAll("li");
     let cseq = clist[seq].getAttribute("data-seq");
     for(let x of indic) x.classList.remove("on");
     indic[cseq].classList.add("on");
-  }; ///// goSlide 함수 ///////
+  };
+
+
   abtn.forEach((ele,idx)=>{
     ele.onclick = () => {
+      clearAuto();
       goSlide(idx);
-    }; /////// click 함수 ///////
-  }); ///// forEach /////////
-}; /////// loadFn함수 ///////
+    };
+  });
+
+  let autoI;
+  let autoT;
+  function autoSlide(){
+    autoI = setInterval(()=>goSlide(1),3000);
+  }
+
+  autoSlide();
+  
+  function clearAuto(){
+    clearInterval(autoI);
+    clearTimeout(autoT);
+    autoT = setTimeout(autoSlide,5000);
+  }
+} 
