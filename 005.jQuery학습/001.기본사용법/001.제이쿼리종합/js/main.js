0d620df809d564ff692a03ca3b85b7256bd0a6a1
dong13475 <travel1347@gmail.com>
2023-04-05 13:20:18 +0900

    Update main.js

--- 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -85,32 +85,92 @@ $(() => {
   btns.hide().first().show();
 
   // 3. 공통함수 : actMini() //////////
-  const actMini = () => {};
-
-  // 4. "들어가기" 버튼 클릭시
-  btns.first()
-  .click(function(){
+  // 전달변수 3개
+  // (1) ele - 클릭된 버튼요소
+  // (2) seq - 이동할 li방순번
+  // (3) fn - 이동후 실행할 코드(콜백함수)
+  const actMini = (ele,seq,fn) => {
+    
     // 1. 클릭된 버튼 사라지기
-    $(this).slideUp(300);
+    $(ele).slideUp(300);
 
     // 2. 메시지 없애기 : .msg -> msg변수
     msg.fadeOut(300);
 
-    // 3. 이동하기
-    // 위치: li 8번방 -> bd변수에 있는 모든 li중 8번
-    let room = bd.eq(8);
+    // 3. 위치값 알아내기
+    // 위치: li 몇번방 -> bd변수에 있는 모든 li중 몇번
+    let room = bd.eq(seq);
     // 위치값 배열변수
     let pos = [];
 
     // top위치값
     pos[0] = room.offset().top;
-    // left위치값
-    pos[1] = room.offset().left;
+    // left위치값 : 방에서 중앙이동(+li가로크기절반-미니언즈가로크기절반)
+    pos[1] = room.offset().left + room.width()/2 - mi.width()/2;
 
-    // 제이쿼리 위치값 정보 메서드 : offset() -> 하위속성:top,left
+    // 제이쿼리 위치값 정보 메서드 : 
+    // offset() -> 하위속성:top,left
+    // 제이쿼리 가로,세로 크기정보 메서드 :
+    // width(), height()
     
     console.log(room,pos);
+
+    // 4. 미니언즈 이동하기
+    // 대상: .mi -> mi변수
+    mi.animate({
+      top: pos[0]+"px",
+      left: pos[1]+"px",
+    },800,"easeOutElastic",
+    fn // 전달된 콜백함수
+    ); //////// animate /////////
+
+  }; //////// actMini함수 //////////
+
+  // 4. "들어가기" 버튼 클릭시
+  btns.first()
+  .click(function(){
+    // 이동후 콜백함수 /////
+    let fn = () => { // 콜백함수
+      // 메시지 넣고 나타나기
+      msg.html("와~아늑하다!<br>옆방으로 가보자!").fadeIn(300);
+
+      // 다음버튼 보이기
+      $(this).next().delay(500).slideDown(300);
+      // this키워드 -> 화살표함수를 사용하여 싸고있는 
+      // 요소인 클릭된 버튼을 가리킴!
+      console.log("여기서 this란",this);
+    }; //////// fn 함수 //////////
+
+    // 공통함수 호출! : this는 클릭된 버튼
+    actMini(this,8,fn);
     
-  }); /////// click //////////
+  }) /////// "들어가기"버튼 click //////////
+  // 5. "옆방으로" 버튼 클릭시
+  .next()
+  .click(function(){
+    let fn = () => { // 콜백함수
+
+      // 좀비 나타나기(2초후)
+      bd.eq(9).find(".mz")
+      .delay(2000)
+      .fadeIn(400,()=>{ // 콜백함수
+        // 메시지 넣고 나타나기
+        msg.html("악!;;;좀비!<br>어서 피하자!")
+        .css({left:"-144%"})
+        .fadeIn(300);
+  
+        // 다음버튼 보이기
+        $(this).next().delay(500).slideDown(300);
+        // this키워드 -> 화살표함수를 사용하여 싸고있는 
+        // 요소인 클릭된 버튼을 가리킴!
+      }); ///////// fadeIn ///////////
+      
+      
+    }; //////// fn 함수 ////////////
+
+    // 공통함수 호출! : this는 클릭된 버튼
+    actMini(this,9,fn);
+    
+  }) /////// "옆방으로"버튼 click ////////////
 
 }); /////////////// jQB ////////////////////
