4434995b98d80cbefe7a24ed775a2c78cfab0f09
dong13475 <travel1347@gmail.com>
2023-04-24 16:24:35 +0900

    학습

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -33,7 +33,7 @@ $(".ham").click(function () {
 
   // 햄버거 버튼에 클래스 on이 있으면 재생 / 없으면 정지
   let isOn = $(this).is(".on");
-  console.log(isOn);
+  // console.log(isOn);
   // 배경동영상 재생 / 멈춤
   if (isOn) $(".bgm").get(0).play();
   else $(".bgm").get(0).pause();
@@ -76,67 +76,91 @@ const reWin = () => $(window).width();
 // 리사이즈 업데이트
 $(window).resize(() => {
   winW = reWin();
-  console.log("winW:", winW);
+  // console.log("winW:", winW);
 });
 
 // 3. 드래그가 끝난후 기준위치 체크후 이동애니메이션
 
-// 윈도우 가로크기 : left 기준위치 px변환! 
+// 윈도우 가로크기 : left 기준위치 px변환!
 let winW = reWin();
-console.log("winW*0.9:", winW*0.9);
-console.log("winW:", winW);
-console.log("winW*1.1:", winW*1.1);
+// console.log("winW*0.9:", winW * 0.9);
+// console.log("winW:", winW);
+// console.log("winW*1.1:", winW * 1.1);
 
 // 광드래그 방지위해 커버셋팅(show()/hide())
 const cover = $(".cover");
 
-
 // 드래그끝난후 이벤트 함수 만들기
-slide.on("dragstop",function(){
-
+slide.on("dragstop", function () {
   // 광드래그 방지위해 커버 보이기
   cover.show();
-  
+
   // 슬라이드 left위치값
   let sleft = $(this).offset().left;
-  console.log("허허",sleft);
+  // console.log("허허", sleft);
 
   // 1. 왼쪽으로 이동 : -110% 미만일때
-  if(sleft < -winW*1.1){
-    slide.animate({
-      left: -winW*2 + "px"
-    },600,"easeOutQuint",()=>{
-      // 이동후 맨앞li 맨뒤이동
-      slide.append(slide.find("li").first())
-      .css({left:"-100%"});
-      // 커버제거하기
-      cover.hide();
-    });
+  if (sleft < -winW * 1.1) {
+    slide.animate(
+      {
+        left: -winW * 2 + "px",
+      },
+      600,
+      "easeOutQuint",
+      () => {
+        // 이동후 맨앞li 맨뒤이동
+        slide.append(slide.find("li").first()).css({ left: "-100%" });
+        // 커버제거하기
+        cover.hide();
+
+        // 배너타이틀함수
+        showTit();
+      }
+    ); //////// animate ///////
+
+    // 블릿이동함수호출!
+    addOn(2);
+    // 왼쪽이동이므로 2번째 슬라이드
   } ////// if : 왼쪽이동 ////////
-  
+
   // 2. 오른쪽으로 이동 : -90% 초과일때
-  else if(sleft > -winW*0.9){
-    slide.animate({
-      left: "0px"
-    },600,"easeOutQuint",()=>{
-      // 이동후 맨뒤li 맨앞으로 이동하기
-      slide.prepend(slide.find("li").last())
-      .css({left:"-100%"});
-      // 커버제거하기
-      cover.hide();
-    }); 
+  else if (sleft > -winW * 0.9) {
+    slide.animate(
+      {
+        left: "0px",
+      },
+      600,
+      "easeOutQuint",
+      () => {
+        // 이동후 맨뒤li 맨앞으로 이동하기
+        slide.prepend(slide.find("li").last()).css({ left: "-100%" });
+        // 커버제거하기
+        cover.hide();
+
+        // 배너타이틀함수
+        showTit();
+      }
+    ); //////// animate ///////
+
+    // 블릿이동함수호출!
+    addOn(0);
+    // 오른쪽이동이므로 0번째 슬라이드
   } ////// else if : 오른쪽이동 ////////
-  
+
   // 3. 제자리로 이동 : -90% 초과일때
-  else{
-    slide.animate({
-      left: -winW + "px"
-    },200,"easeOutQuint",()=>{
-      // 커버제거하기
-      cover.hide();
-    });
+  else {
+    slide.animate(
+      {
+        left: -winW + "px",
+      },
+      200,
+      "easeOutQuint",
+      () => {
+        // 커버제거하기
+        cover.hide();
+      }
+    );
   } ////// else : 제자리이동 ////////
-  
 }); ///////// slide //////////
 
 /********************************************* 
@@ -154,19 +178,160 @@ const blist = slide.find("li");
 // 배너 li개수
 const bcnt = blist.length;
 
-blist.each((idx,ele)=>{
-  console.log(idx,bcnt);
+blist.each((idx, ele) => {
+  // console.log(idx, bcnt);
   // 처음것을 마지막 순번으로 넣기
-  if(idx===0)
-    $(ele).attr("data-seq",bcnt-1);
+  if (idx === 0) $(ele).attr("data-seq", bcnt - 1);
   // 두번째 부터 끝까지 0부터(1작음!)
-  else 
-    $(ele).attr("data-seq",idx-1);
+  else $(ele).attr("data-seq", idx - 1);
 }); ///////// each ///////////
 
-/************************** 
-  블릿 on 넣기 함수
-**************************/
-function addOn(){
-  
+/********************************** 
+  [ 블릿 on 넣기 함수 ]
+
+  1) 오른쪽으로 이동일경우
+  -> 0번째 슬라이드의 data-seq값
+  2) 왼쪽으로 이동일경우
+  -> 2번째 슬라이드의 data-seq값
+  3) 위의 선택값으로 블릿의 
+  li순번에 on넣고 나머지는 뺀다!
+**********************************/
+// 대상선정: .bindic li
+const bindic = $(".bindic li");
+
+function addOn(seq) {
+  // seq - 읽을 슬라이드 순번
+  // 방향은 어떻게 알지?
+  // 0은 오른쪽이동, 2는 왼쪽이동임!
+
+  // 1. 해당슬라이드 data-seq읽어오기
+  let dseq = slide.find("li").eq(seq).attr("data-seq");
+  // console.log("ㅁㄴㅇ", dseq);
+
+  // 2. 해당슬라이드와 동일한 순번블릿에 on넣기
+  bindic.eq(dseq).addClass("on").siblings().removeClass("on");
 } ////////// addOn 함수 //////////
+
+///////////////////////////////////////
+////// 각 배너 등장 타이틀 셋팅 /////////
+///////////////////////////////////////
+let bantxt = {
+  ban1: "Men's Season<br>Collection",
+  ban2: "2023 Special<br>Collection",
+  ban3: "GongYoo<br>Collection",
+  ban4: "T-Shirt<br>Collection",
+  ban5: "Shoes<br>Collection",
+  ban6: "Wind Jacket<br>Collection",
+}; ///////////// bantxt객체 //////////////
+
+/************************************** 
+  함수명 : showTit
+  기능 : 각 배너 타이틀 보이기
+  호출 : 배너이동후 콜백함수에서 호출함
+**************************************/
+function showTit() {
+  // 요구사항 : 배너이동후 호출하여
+  // 해당배너의 순번에 맞는 타이틀을
+  // 동적으로 생성하여 애니메이션 한다!
+
+  // 주인공배너
+  const mainban = slide.find("li").eq(1);
+
+  // 1. 항상 도착후엔 두번째 슬라이드가 주인공이다!
+  // 슬라이드 순번은 1번!
+  // 슬라이드 클래스명 읽어오기(타이틀이 클래스명과 연관됨!)
+  let clsnm = mainban.attr("class");
+
+  // 2. 클래스명에 해당하는 객체값 읽어오기
+  let bantit = bantxt[clsnm];
+
+  // 호출확인
+  console.log("배너타이틀!", clsnm, bantit);
+
+  // 모든 추가 타이틀 지우기
+  $(".btit").remove();
+
+  // 3. 타이틀을 넣을 요소를 배너에 추가한다!
+  mainban.append(`<h2 class="btit"></h2>`);
+
+  // 타이틀 left위치 변수처리
+  // ban2, ban3만 오른쪽위치
+  let lval = "30%";
+  if (clsnm === "ban2" || clsnm === "ban3") lval = "70%";
+
+  // 4. 해당배너 h2태그에 배너 타이틀 넣기
+  mainban
+    .find(".btit")
+    .html(bantit)
+    .css({
+      position: "absolute",
+      top: "55%", // 약간아래
+      left: lval,
+      transform: "translate(-55%,-50%)",
+      font: "bold 4.5vmax Verdana",
+      color: "#fff",
+      textShadow: "1px 1px 3px #777",
+      whiteSpace: "nowrap",
+      opacity: 0, // 처음에 투명
+    }) /////// css ////////
+    .animate(
+      {
+        /////// 등장애니메이션~!!!
+        top: "50%",
+        opacity: 1,
+      },
+      1000,
+      "easeInOutQuart"
+    );
+} ////////// showTit ////////////
+
+// 첫번째 배너를 위한 타이틀함수 최초호출!
+setTimeout(showTit, 1000);
+
+// 타임아웃변수
+let banAgain;
+
+
+// 자동넘김 지우기 함수
+const clearAuto = () => {
+  clearInterval(banAuto);
+  clearTimeout(banAgain);
+  banAgain = setTimeout(banAutoSlide,5000);
+  
+}; ///////// clearAuto 함수 ///////////
+
+// 배너이동시 자동넘기 지우기 셋팅
+slide.on("mousemove dragstart dragstop",clearAuto);
+
+// 자동넘김 인터발 셋팅하기 ///////
+// 변수에 담아 정지하기 //////
+let banAuto;
+
+const banAutoSlide = () => {
+  banAuto = setInterval(() => {
+    slide.animate(
+      {
+        left: -winW * 2 + "px",
+      },
+      600,
+      "easeOutQuint",
+      () => {
+        // 이동후 맨앞li 맨뒤이동
+        slide.append(slide.find("li").first()).css({ left: "-100%" });
+        // 커버제거하기
+        cover.hide();
+
+        // 배너타이틀함수
+        showTit();
+      }
+    ); //////// animate ///////
+
+    // 블릿이동함수호출!
+    addOn(2);
+    // 왼쪽이동이므로 2번째 슬라이드
+  }, 3000);
+}; //////// banAutoSlide 함수 //////////
+
+// 자동넘김 최초호출!
+banAutoSlide();
+
