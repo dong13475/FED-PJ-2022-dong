b2b330adb104826e9e02117e9e7b04375dc90b5f
dong13475 <travel1347@gmail.com>
2023-05-17 13:05:43 +0900

    학습

---
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-store.js
@@ -0,0 +1,51 @@
+// 전체 리스트 페이지 뷰엑스 스토어 셋팅 JS - glist-store.js
+
+// 전체 상품정보 불러오기
+import gdata from "./gdsData/glist-items.js";
+
+const store = new Vuex.Store({
+  state: {
+    // 서브데이터 셋업
+    // 전체상품정보 전역화
+    gdata:gdata,
+    // 필터데이터용 배열변수
+    chkarr:[true,true,true],
+    // 필터데이터용 배열입력값 변수
+    selnm:["","",""],
+    // 페이징용 변수
+    pnum:0,
+  },
+  // state 데이터 변경 메서드구역!
+  mutations: {
+    // 체크박스 체크시 처리메서드
+    resCheck(dt){
+      console.log(dt.chkarr);
+      // 3개의 체크박스 상태배열변수값에 따라
+      // 실제 조건에 들어갈 cat명을 넣어준다!
+
+      // v는 배열값인 true/false값이 들어옴!
+      dt.chkarr.forEach((v,i) => {
+        if(v){ // 체크박스 체크시
+          dt.selnm[i] = 
+            i==0?"men":i==1?"women":"style";
+            // 조건1?값1:((조건2?값2):최종값);
+            // 중첩3항연산자 사용!
+        }
+        else{ // 체크박스 체크안됨
+          // 무조건 빈값을 할당!!!
+          dt.selnm[i] = "";
+        }
+      });
+      
+    }, ////// resCheck //////
+
+    // 페이징 변수 업데이트 메서드
+    updatePaging(dt,pm){ // pm - 업데이트할 전달숫자
+      // pnum은 리스트 범위수
+      dt.pnum = pm;
+    }, /////// updatePaging ///////
+  },
+});
+
+// 내보내기!
+export default store;
--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist.js
@@ -5,10 +5,45 @@
 import menuFn from "./mainjs/menu.js";
 // 전체리스트 태그 데이터 가져오기
 import comData from "./tempData/data-glist.js";
-// 신상정보
-import sinsang from "./gdsData/sinsang.js";
 
-// 뷰엑스 스토어 JS 가져오기
-// 중요! 반드시 메인JS파일 한군데서 불러와 써야 상태관리됨!
-// -> 이 JS파일에 Vue 인스턴스 생성코드가 같이 있어야한다!
-import store from "./store.js";
+
+// 전체 리스트용 뷰엑스 스토어 JS 가져오기
+import store from "./glist-store.js";
+
+// 전체 리스트용 뷰 라우터 JS 가져오기 
+import router from "./glist-router.js";
+
+// 1. 뷰 템플릿 만들기
+//###### 상단영역 메뉴 뷰 템플릿 셋팅하기 #######
+// Vue.component(내가지은요소명,{옵션})
+Vue.component("top-comp", {
+  template: comData.tarea,
+  methods: {},
+}); ////////// 상단영역 Vue component //////////
+
+//###### 하단영역 메뉴 뷰 템플릿 셋팅하기 #######
+Vue.component("foot-comp", {
+  template: comData.barea,
+}); ////////// 하단영역 Vue component //////////
+
+// 2. 뷰 인스턴스 생성하기
+new Vue({
+  el:".wrap",
+  store, // 스토어등록!
+  router, // 라우터등록!
+  mounted(){
+    // 첫번째 라우터 강제실행!
+    this.$router.push('/glist');
+    // push(실행할 뷰라우터경로)
+    // $router - 전체 라우터객체
+    // 비교) $route - 개별경로정보객체
+
+    // 최초 체크박스체크 메서드 실행해야 리스트나옴!
+    store.commit('resCheck');
+
+    // 메뉴기능실행
+    menuFn();
+    // 로고이동기능
+    $("#logo").click(() => (location.href = "index.html"));
+  }, //////// mounted //////////
+})
