47207f2d94828735e106f4eb413f49090e570885
dong13475 <travel1347@gmail.com>
2023-06-05 11:44:27 +0900

    학습

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Movies.js
@@ -0,0 +1,13 @@
+// DC 무비 페이지 컴포넌트
+import React from "react";
+import Ban from "./Ban";
+
+const Movies = () => {
+  return(
+    <>
+      <Ban cat="MOVIES" />
+    </>
+  );
+}; ////////////// Movies /////////////
+
+export default Movies;
