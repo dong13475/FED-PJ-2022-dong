ad1891c00cf280e4e6980756f79250e17a3ade87
dong13475 <travel1347@gmail.com>
2023-06-05 16:56:13 +0900

    학습

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
@@ -1,11 +1,20 @@
 // DC 메인 페이지 컴포넌트
 import React from "react";
 import Ban from "./Ban";
+import MenuBtn from "./MenuBtn";
+import menubtn_data from "./data/menubtn";
 
 const Main = () => {
   return(
     <>
       <Ban cat="main" />
+      <div className="fbox">
+        {
+          menubtn_data.map((x,i)=>{
+            return <MenuBtn rec={x} key={i} />
+          })
+        }
+      </div>
     </>
   );
 }; ////////////// Main /////////////
---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/menubtn.js
@@ -0,0 +1,31 @@
+// MenuBtn 컴포넌트용 데이터 - menubtn.js
+
+const menubtn_data = [
+  {
+    "isrc":"./images/menubtn1.jpg",
+    "tit":"WORLDS COLLIDE^THE FLASH IS ONLY IN THEATERS JUNE 16",
+    "btn":"TICKET ON SALE NOW",
+    "link":"/co",
+  },
+  {
+    "isrc":"./images/menubtn2.jpg",
+    "tit":"DC SHOP^GET NUTS WITH THE 1/6 SCALE BATMAN FIGURE",
+    "btn":"SHOP NEW ARRIVALS",
+    "link":"/co",
+  },
+  {
+    "isrc":"./images/menubtn3.jpg",
+    "tit":"DC PRIDE^CELEBRATE WITH LGBTQ CREATORS, CHARACTERS, & STORIES",
+    "btn":"VISIT OUR PRIDE HUB",
+    "link":"/co",
+  },
+  {
+    "isrc":"./images/menubtn4.jpg",
+    "tit":"DC COMMUNITY^SHARE YOUR DC PRIDE FANWORKS",
+    "btn":"SUBMIT YOUR ART",
+    "link":"/co",
+  },
+]
+
+// 내보내기
+export default menubtn_data;
